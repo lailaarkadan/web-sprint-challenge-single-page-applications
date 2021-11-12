87d0e6ba2e8ded086fa40f8838b1c7aa2b6f841d
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-12 09:55:08 -0700

    creating Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Form from "./Components/Form";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+    <div className="App">
+      <nav>
+        <div className="header">Lambda Eats</div>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Order Pizza</Link>
+      </nav>
+      <Switch>
+        <Route path='/pizza'>
+          <Form
+    
+          />
+        </Route>
+      </Switch>
+
+    </div>
+    </Router>
   );
-};
+}
 export default App;
